d43c7b2178e361266b70bbccc705136a6bf7278b
Andrew Reifers <areifers@uw.edu>
2021-04-16 15:30:56 -0700

    Added additional comments and error handling to make code easier to understand and more robust before trying to put onto an actual device.

--- Main.py
+++ Main.py
@@ -9,20 +9,30 @@ open_hours = {"opening_time": "9:00AM", "closing_time": "9:00PM"}
 
 front_door = smart_door(home_topic_name, home_access_code, open_hours)
 
+def on_connect(client, userdata, flags, rc):
+    print("Connected to " + broker + " with result code "+str(rc))
+
+
 #define on_message_callback
 def on_message(client, userdata, message):
+    print("on_message function called.")
+    print("Message Topic: " + message.topic + " Message: " + str(message.payload))
     front_door.process_published_message(message)
 
 #create client object
 client = paho.Client(home_topic_name + home_access_code)
+client.on_connect = on_connect
 ######Bind function to callback
 client.on_message = on_message
 
 print("connecting to home security MQTT broker ", broker)
 client.connect(broker)
 
-for x in front_door.topic_subs:
-    print(home_topic_name + " front door client subscribing to " + x)
-    client.subscribe(x)
+#loop through the topics property of the front door and subscribe
+for topic in front_door.topic_subs:
+    #Print out the topic that is being subscribed to
+    print("Reifers Smart door client subscribing to " + topic)
+    #Use the Paho client to subscribe to the topic
+    client.subscribe(topic)
 
 client.loop_forever()
--- reifersSmartDoor.py
+++ reifersSmartDoor.py
@@ -2,8 +2,14 @@ import datetime
 import json
 from types import SimpleNamespace
 
+# Example mosquitto_pub Commands
+# mosquitto_pub -h uwischoolreifers.westus2.cloudapp.azure.com -t infosec/IoTurity/home/reifers/front-door -m "{ \"state_change_request\" : \"unlock\", \"pin_code\": \"1337\"}"
+# mosquitto_pub -h uwischoolreifers.westus2.cloudapp.azure.com -t infosec/IoTurity/home/reifers/time -m "{ \"current_time\": \"12:44:11\" }"
+# mosquitto_pub -h uwischoolreifers.westus2.cloudapp.azure.com -t infosec/IoTurity/home/reifers/emergency -m "{ \"emergency_code\": \"silver\" }"
+
 class smart_door(object):
 
+    #Constructor for smart_door class
     def __init__(self, home_name, code, open_hours):
         self.topic_subs = [(home_name + "/front-door"),
                            (home_name + "/emergency"), (home_name + "/time")]
@@ -19,13 +25,16 @@ class smart_door(object):
         pass
 
     def set_current_time(self, time_payload):
+        print('Setting Current Time')
         try:
             self.current_time = datetime.datetime.strptime(self.current_time.strftime('%d/%m/%Y') + ' ' + time_payload.current_time, "%d/%m/%Y %H:%M:%S")
+            print('Time Set to: ' + self.current_time.strftime(self.date_format))
         except Exception as e:
-            print('Unable to set time to the Time provided')
+            print("Unable to set time due to following Error: ")
             print(e)
 
     def check_pin_code(self, payload):
+        print('Validating Pin Code')
         if hasattr(payload, 'pin_code'):
             pin_matches = payload.pin_code == self.pin_code
             if pin_matches:
@@ -39,14 +48,17 @@ class smart_door(object):
             return False
 
     def unlock(self):
+        print('Unlocking Door')
         self.lock_status = 'unlocked'
         self.last_status_change = datetime.datetime.now()
 
     def lock(self):
+        print('Locking Door')
         self.lock_status = 'locked'
         self.last_status_change = datetime.datetime.now()
 
     def emergency_response(self, payload):
+        print('Processing Emergency Code')
         # National Emergency Response Codes
         # Todo: These codes need to be validated and operations will need to make sure that they stay up to date
         if payload.emergency_code == 'silver' or payload.emergency_code == 'white' or payload.emergency_code == 'pink':
@@ -72,8 +84,7 @@ class smart_door(object):
         else:  # crosses midnight
             return check_time >= begin_time or check_time <= end_time
 
-#{ "state_change_request" : "locked", "pin_code": "1337"}
-#{ \"current_time\": \"12:44:11\" }
+
     def process_published_message(self, message):
         try:
             payload_string = str(message.payload.decode("utf-8"))
@@ -81,7 +92,7 @@ class smart_door(object):
             try:
                 payload_json = json.loads(payload_string, object_hook=lambda d: SimpleNamespace(**d))
             except ValueError as json_parse_error:
-                print("Unable to Parse JSON. Error Message: " + json_parse_error)
+                print("Unable to Parse JSON. Error Message: " + str(json_parse_error))
             print("Front Door processing Topic = ", message.topic)
             print("Front Door processing message =", payload_string)
             if message.topic.endswith('emergency'):
@@ -100,4 +111,5 @@ class smart_door(object):
                             self.lock()
             self.print_door_status()
         except Exception as e:
-            print("Uncaught Error Occurred.  Error Type : " + type(e) + ". Error Args : " + e.args)
+            print("Uncaught Error Occurred.  Error Type : " + str(type(e)) + ". Error:")
+            print(e)
